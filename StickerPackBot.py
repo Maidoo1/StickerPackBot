6fe8109d6c5bd1c4e858fef5cdf69157104ee244
Maidoo1 <exxelpwnz@mail.ru>
2017-08-12 04:26:43 +0500

    send png file in chat

--- StickerPackBot.py
+++ StickerPackBot.py
@@ -13,14 +13,20 @@ def start(message):
 
 @bot.message_handler(content_types=['sticker'])
 def create_pack(message):
+    print(message)
+    print(message.chat.id)
     file_info = bot.get_file(message.sticker.file_id)
     print(file_info)
     file_download = bot.download_file(file_info.file_path)
     print(file_download)
-    src = file_info.file_path
+    src = file_info.file_path[:-4] + 'png'
 
+    print(src)
     with open(src, 'wb') as new_file:
         new_file.write(file_download)
+
+    with open(src, 'rb') as send_file:
+        bot.send_document(message.chat.id, send_file)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
