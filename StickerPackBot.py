43683117a49a9a447d361239c6f50b7a34854ef3
Maidoo1 <exxelpwnz@mail.ru>
2017-08-16 01:12:54 +0500

    added stickers test function

--- StickerPackBot.py
+++ StickerPackBot.py
@@ -27,12 +27,18 @@ class Sticker:
     def create_sticker_pack(self, message):
         src = self.upload_sticker(message)
         self.pack_name = self.pack_name + '_by_stickerpackbot'
+
         with open(src, 'rb') as send_file:
             bot.create_new_sticker_set(self.user_id, self.pack_name,
                                        self.pack_title, send_file, message.sticker.emoji)
-            bot.send_message(message.chat.id, 't.me/addstickers/' + self.pack_name)
 
         # os.remove(src)
+
+    def add_sticker_to_pack(self, message):
+        src = self.upload_sticker(message)
+
+        with open(src, 'rb') as send_file:
+            bot.add_sticker_to_set(message.from_user.id, self.pack_name, send_file, message.sticker.emoji, None)
 
 
 user_dict = {}
@@ -50,26 +56,50 @@ def start(message):
 @bot.message_handler(commands=['pack_from_stickers'])
 def pack_from_stickers_name(message):
     user_dict[message.chat.id] = Sticker(message.chat.id)
+
     request = bot.send_message(message.chat.id, 'Pass the sticker pack name')
     bot.register_next_step_handler(request, sticker_pack_name)
 
 
 def sticker_pack_name(message):
     user_dict[message.chat.id].pack_name = str(message.text)
+
     request = bot.send_message(message.chat.id, 'Pass the sticker pack title')
     bot.register_next_step_handler(request, sticker_pack_title)
 
 
 def sticker_pack_title(message):
     user_dict[message.chat.id].pack_title = str(message.text)
+
     request = bot.send_message(message.chat.id, 'Send 1 sticker')
     bot.register_next_step_handler(request, create_pack)
 
 
-@bot.message_handler(content_types=['sticker'])
+@bot.message_handler(content_types=['text, sticker'])
 def create_pack(message):
     user_dict[message.chat.id].create_sticker_pack(message)
 
-    
+    request = bot.send_message(message.chat.id, 'Send sticker')
+    bot.register_next_step_handler(request, add_sticker)
+
+
+def add_sticker(message):
+    if str(message.text) == '/stop':
+        return bot.send_message(message.chat.id, 't.me/addstickers/' + user_dict[message.chat.id].pack_name)
+
+    user_dict[message.chat.id].add_sticker_to_pack(message)
+    request = bot.send_message(message.chat.id, 'sticker has been added')
+    bot.register_next_step_handler(request, add_sticker_1)
+
+
+def add_sticker_1(message):
+    if str(message.text) == '/stop':
+        return bot.send_message(message.chat.id, 't.me/addstickers/' + user_dict[message.chat.id].pack_name)
+
+    user_dict[message.chat.id].add_sticker_to_pack(message)
+    request = bot.send_message(message.chat.id, 'Send sticker')
+    bot.register_next_step_handler(request, add_sticker)
+
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
